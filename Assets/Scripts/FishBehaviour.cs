cc5d6cdc9d38d631f7b46ef805d2c6859f51b253
Connor L_Lab101 <114543523+IAmMrLuck@users.noreply.github.com>
2023-06-23 15:54:40 +1000

    added in the background, started on the prefabs for fish and rod

--- Assets/Scripts/FishBehaviour.cs
+++ Assets/Scripts/FishBehaviour.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class FishBehaviour : MonoBehaviour
+namespace ConaLuk
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class FishBehaviour : MonoBehaviour
     {
-        
+
+        [SerializeField] private GameObject fish;
+        [SerializeField] private GameObject lineCatch;
+
+        private bool onHook = false;
+
+        // animations for line
+        // 
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // fishgame.Play();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // is the fish on a hook
+            // if it is, then prompt player to reel it in
+
+            // reduce alpha on the bridge
+            // the logic for the reel will be in the fishing rod
+
+
+            // if not, prompt player to through line
+
+        }
+
+
+
     }
 }
